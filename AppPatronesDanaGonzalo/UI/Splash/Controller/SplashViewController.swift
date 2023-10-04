1d6cbe377492eca7b737642cdb1ab15fbc9535cb
Dana Gonzalo <damdgonzalo@gmail.com>
2023-10-04 22:46:05 +0200

    Added LaunchScreen image

--- AppPatronesDanaGonzalo/UI/Splash/Controller/SplashViewController.swift
+++ AppPatronesDanaGonzalo/UI/Splash/Controller/SplashViewController.swift
@@ -1,18 +1,17 @@
 import UIKit
 
 
-//MARK: - PROTOCOLO -
+//MARK: - Protocol
 protocol SplashViewProtocol: AnyObject {
     func showLoading(_ show: Bool)
     func navigateToHome()
 }
 
 
-
-//MARK: - CLASE -
+//MARK: - Class
 class SplashViewController: UIViewController {
 
-    @IBOutlet weak var activituIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var viewModel: SplashViewModelProtocol?
     
@@ -23,26 +22,24 @@ class SplashViewController: UIViewController {
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        activituIndicator.stopAnimating()
+        activityIndicator.stopAnimating()
     }
 }
 
 
 
-//MARK: - EXTENSION -
+//MARK: - Extension
 extension SplashViewController: SplashViewProtocol {
-    //Metodo cargar activity indicator
     func showLoading(_ show: Bool) {
         switch show {
-        case true where !activituIndicator.isAnimating:
-            activituIndicator.startAnimating()
-        case false where activituIndicator.isAnimating:
-            activituIndicator.stopAnimating()
+        case true where !activityIndicator.isAnimating:
+            activityIndicator.startAnimating()
+        case false where activityIndicator.isAnimating:
+            activityIndicator.stopAnimating()
         default: break
         }
     }
     
-    //Metodo para navegar a la home
     func navigateToHome() {
         let nextVC = HomeTableViewController()
         nextVC.viewModel = HomeViewModel(viewDelegate: nextVC)
